7c4ad5140a1344390d9d3af44857f38f7c506e39
char <charbel@hotmail.com>
2024-03-28 08:27:48 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const xlsx=require('xlsx');
+const _PORT= process.env._PORT||3000
+app.use(cors())
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+app.get('/services',(req,res)=>{
+    let wb=xlsx.readFile('data.xlsx');
+    let ws=wb.Sheets['services'];
+    let data =xlsx.utils.sheet_to_json(ws);
+//  let data=[
+// {title:'1',description:'desc1',icon:'briefcase'},
+//  {title:'2',description:'desc2',icon:'envelope'},
+//  {title:'3',description:'desc3',icon:'gear'},
+//  {title:'4',description:'desc4',icon:'map'},
+//  {title:'5',description:'desc5',icon:'cloud'},
+//  {title:'6',description:'desc6',icon:'twitter'}
+// ]  
+ res.send(data); 
+})
+app.listen(3000)
